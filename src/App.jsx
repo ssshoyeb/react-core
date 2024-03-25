dcef57a44f285cc75836810db620ce7456b16150
ssshoyeb <shamimshahariashoyeb@gmail.com>
2024-03-25 22:33:54 +0600

    chill

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import Countries from './components/countries/Countries'
 import './App.css'
-
 function App() {
 
   return (
     <>
       <h1>React World Tour</h1>
+      <Countries></Countries>
+      
     </>
   )
 }
